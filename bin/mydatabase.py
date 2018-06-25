c20fbd3fcd1ea8ca53c1cd461bb0e7ec97b92ec9
Kevin Yin <keviny.whs@gmail.com>
2018-06-24 17:31:13 -0700

    added create table functionality

--- bin/mydatabase.py
+++ bin/mydatabase.py
@@ -1,7 +1,9 @@
 import sqlite3
+import csv
 sqlite_file = 'my_db.sqlite'
 
 class MyDatabase:
+
     sqlite_file = 'my_db.sqlite'
 
     def __init__(self):
@@ -10,11 +12,31 @@ class MyDatabase:
 
     def create_table(self, schema, name):
         self.c.execute("DROP TABLE IF EXISTS " + name)
-        
+        sql_create_table = "CREATE TABLE IF NOT EXISTS " + name + "("
+
+        with open(schema, "r") as f:
+            reader = csv.reader(f, delimiter = "\t")
+            next(f)
+            for line in (reader):
+                try:
+                    field_name, width, type = line[0].split(',')
+                except ValueError:
+                    print("Value for this line does not have three fields")
+                    continue
+
+                sql_create_table += " {} {}({}),".format(field_name, type, width)
+
+        sql_create_table= sql_create_table.rstrip(",")
+        sql_create_table += ");"
+        self.c.execute(sql_create_table)
+        self.conn.commit()
 
 
 
 
-MyDatabase()
+
+x = MyDatabase()
+x.create_table('schema.csv', 'authors')
+x.conn.close()
 
 
