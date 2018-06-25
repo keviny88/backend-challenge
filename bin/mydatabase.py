c21c37011ce1b018e6bcbd3eb25b06a61d3e280f
Kevin Yin <keviny.whs@gmail.com>
2018-06-24 17:54:34 -0700

    added get headers function for code reuse

--- bin/mydatabase.py
+++ bin/mydatabase.py
@@ -34,19 +34,27 @@ class MyDatabase:
     # Creating a a visual representation of the table
     def show_table(self, name):
         # Extract the header names and print them out
-        self.c.execute("PRAGMA table_info({})".format(name))
         header = ""
-        rows = self.c.fetchall()
-        for row in rows:
-            header += row[1] + "  | "
+        for title in self.get_headers(name):
+            header += title + "  | "
         print(header)
 
+
+    def insert_data(self, data_file):
+        pass
+
+
+    # Gets headers for a table and returns them in a list
+    def get_headers(self, name):
+        self.c.execute("PRAGMA table_info({})".format(name))
+        return [row[1] for row in self.c.fetchall()]
 
 
 
 x = MyDatabase()
 x.create_table('schema.csv', 'authors')
-x.show_table('authors')
+print(x.get_headers('authors'))
+# x.show_table('authors')
 x.conn.close()
 
 
